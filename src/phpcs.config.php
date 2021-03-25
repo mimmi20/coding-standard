6107b007d0bccc408c3c8e9d160d11492aa88306
Thomas Müller <mimmi20@live.de>
2021-03-25 21:36:43 +0100

    update rules for php-cs-fixer

--- src/phpcs.config.php
+++ src/phpcs.config.php
@@ -11,16 +11,11 @@
 declare(strict_types = 1);
 
 return [
-    '@PSR2' => true,
+    '@PSR12' => true,
     '@PhpCsFixer' => true,
     '@PhpCsFixer:risky' => true,
     '@Symfony' => true,
     '@Symfony:risky' => true,
-    '@PHP70Migration' => true,
-    '@PHP70Migration:risky' => true,
-    '@PHP71Migration' => true,
-    '@PHP71Migration:risky' => true,
-    '@PHP73Migration' => true,
     '@PHP74Migration' => true,
     '@PHP74Migration:risky' => true,
     '@PHP80Migration' => true,
@@ -29,8 +24,7 @@ return [
     '@PHPUnit75Migration:risky' => true,
     '@PHPUnit84Migration:risky' => true,
 
-    // @PSR2 rules configured different from default
-    'blank_line_after_namespace' => true,
+    // @PSR12 rules configured different from default
     'class_definition' => [
         'single_line' => false,
         'single_item_single_line' => true,
@@ -41,38 +35,47 @@ return [
         'keep_multiple_spaces_after_comma' => false,
     ],
     'no_break_comment' => false,
-    'visibility_required' => ['elements' => ['property', 'method', 'const']],
 
     // @PhpCsFixer rules configured different from default
     'align_multiline_comment' => ['comment_type' => 'all_multiline'],
-    'array_syntax' => ['syntax' => 'short'],
-    'binary_operator_spaces' => ['default' => 'single_space', 'operators' => ['=' => 'align_single_space_minimal']],
+    'binary_operator_spaces' => [
+        'default' => 'single_space',
+        'operators' => ['=' => null, '-=' => null, '.=' => null],
+    ],
     'concat_space' => ['spacing' => 'one'],
     'declare_equal_normalize' => ['space' => 'single'],
+    'phpdoc_order' => false,
+    'phpdoc_separation' => false,
     'php_unit_internal_class' => false,
-    'no_superfluous_phpdoc_tags' => false,
+    'no_superfluous_phpdoc_tags' => ['allow_mixed' => true, 'allow_unused_params' => true, 'remove_inheritdoc' => true],
     'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
     'no_extra_blank_lines' => [
-        'tokens' => ['break', 'case', 'continue', 'curly_brace_block', 'default', 'extra', 'parenthesis_brace_block', 'return', 'square_brace_block', 'switch', 'throw', 'use', 'useTrait', 'use_trait'],
+        'tokens' => [
+            'break',
+            'case',
+            'continue',
+            'curly_brace_block',
+            'default',
+            'extra',
+            'parenthesis_brace_block',
+            'square_brace_block',
+            'switch',
+            'throw',
+            'useTrait',
+            'use_trait',
+        ],
     ],
-    'no_useless_return' => false,
-    'no_whitespace_before_comma_in_array' => ['after_heredoc' => false],
     'php_unit_test_class_requires_covers' => false,
-    'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
+    'phpdoc_add_missing_param_annotation' => false,
     'phpdoc_no_empty_return' => false,
     'phpdoc_summary' => false,
-    'single_blank_line_before_namespace' => false,
     'single_line_comment_style' => ['comment_types' => ['hash']],
-    'trailing_comma_in_multiline_array' => ['after_heredoc' => false],
 
     // @PhpCsFixer:risky rules configured different from default
-    'php_unit_strict' => ['assertions' => ['assertAttributeEquals', 'assertAttributeNotEquals', 'assertNotEquals']],
-    'no_alias_functions' => ['sets' => ['@internal', '@IMAP', '@mbreg', '@all']],
-    'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+    'no_alias_functions' => ['sets' => ['@all']],
 
     // @Symfony rules configured different from default
     'no_blank_lines_after_phpdoc' => false,
-    'space_after_semicolon' => ['remove_in_empty_for_expressions' => true],
     'yoda_style' => [
         'equal' => true,
         'identical' => true,
@@ -80,61 +83,49 @@ return [
     ],
     'single_line_throw' => false,
 
-    // @Symfony:risky rules configured different from default
-    'non_printable_character' => ['use_escape_sequences_in_strings' => true],
-
-    // @PHP70Migration rules configured different from default
-    'ternary_to_null_coalescing' => false,
-
-    // @PHP70Migration:risky rules configured different from default
-    'pow_to_exponentiation' => false,
-
     // @PHPUnit60Migration:risky rules configured different from default
     'php_unit_dedicate_assert' => ['target' => 'newest'],
 
-    //@PHP73Migration rules configured different from default
-    'heredoc_indentation' => false,
-
     // other rules
-    'backtick_to_shell_exec' => true,
-    'class_keyword_remove' => false,
     'final_class' => true,
-    'final_internal_class' => [
-        'annotation-black-list' => ['@final', '@Entity', '@ORM'],
-        'annotation-white-list' => ['@internal'],
-    ],
     'final_public_method_for_abstract_class' => true,
-    'final_static_access' => true,
     'general_phpdoc_annotation_remove' => [
         'expectedExceptionMessageRegExp',
         'expectedException',
         'expectedExceptionMessage',
         'author',
+        'package',
+        'subpackage',
     ],
-    'global_namespace_import' => false,
-    'linebreak_after_opening_tag' => true,
-    'list_syntax' => ['syntax' => 'short'],
+    'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
     'mb_str_functions' => true,
     'native_constant_invocation' => false,
     'native_function_invocation' => false,
-    'no_blank_lines_before_namespace' => true,
-    'no_null_property_initialization' => true,
-    'no_php4_constructor' => true,
-    'not_operator_with_space' => false,
-    'not_operator_with_successor_space' => false,
+    'single_blank_line_before_namespace' => true,
     'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => true],
-    'ordered_class_elements' => false,
-    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'ordered_class_elements' => [
+        'use_trait',
+        'constant_public',
+        'constant_protected',
+        'constant_private',
+        'property_public',
+        'property_protected',
+        'property_private',
+        'construct',
+        'destruct',
+        'magic',
+        'phpunit',
+        'method_public',
+        'method_protected',
+        'method_private',
+    ],
+    'ordered_imports' => false,
     'ordered_interfaces' => ['direction' => 'ascend', 'order' => 'alpha'],
-    'php_unit_size_class' => false,
     'php_unit_test_annotation' => ['case' => 'camel', 'style' => 'prefix'],
-    'phpdoc_to_param_type' => false,
-    'phpdoc_to_return_type' => false,
     'phpdoc_types_order' => [
         'null_adjustment' => 'always_last',
         'sort_algorithm' => 'alpha',
     ],
     'self_static_accessor' => true,
-    'simplified_null_return' => false,
     'static_lambda' => true,
 ];
